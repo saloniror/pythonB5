466420301a1ccfdb6aef514d894ae120000d7bc2
saloniror <salonichaudhary9368@gmail.com>
2024-09-23 16:42:37 +0530

    copied old files

---
+++ old files/untitled.py
@@ -0,0 +1,5 @@
+  # Get the current time in seconds since the epoch (Unix timestamp)
+  current_time = time.time()
+  print("Current time (in seconds):", current_time)
+
+
---
+++ old files/untitled1.py
@@ -0,0 +1,6 @@
+import time
+
+  print("Start")
+  time.sleep(2)  # Pauses execution for 2 seconds
+  print("End after 2 seconds")
+  
